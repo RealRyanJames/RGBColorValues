69f0cbb38226bb47b66cc9c1099cb89cf45ace83
RealRyanJames <realcoder2023@gmail.com>
2026-09-14 20:16:14 -0400

    added a function title and function to print substring of title or message and log to console

--- Main.cpp
+++ Main.cpp
@@ -9,26 +9,59 @@ std::map<std::string, float> NumbersGenerated(float r, float g, float b)
     return colorsCodedX;
 }
 
-auto main() -> int
+std::string Title(std::string str)
+{
+    return str.substr(0, str.size() + 1);
+}
+
+std::string appendTextLog(std::string logMessage, std::string titleMessage)
+{
+    return logMessage.append(titleMessage);
+}
+
+auto printMessage(std::string messageLogger) -> std::string
+{
+    return appendTextLog(messageLogger, "\n");
+}
+
+auto setup() -> void
 {
 
-    // Gets Length of Elements in Maps Data Structure
+    std::string title = "Welcome Users!\n";
 
-    float r = rand() % 254 + 1;
-    float g = rand() % 254 + 1;
-    float b = rand() % 254 + 1;
-
-    std::map<std::string, float>
-        colorsCoded = NumbersGenerated((int)r, (int)g, (int)b);
-
-    for (int i = 0; i < 1; i++)
+    if (Title(title) == title)
     {
-        for (auto color : colorsCoded)
+        std::cout << appendTextLog(title, "Created by ZumbaCodez") << std::endl;
+
+        // Gets Length of Elements in Maps Data Structure
+        float r = rand() % 254 + 1;
+        float g = rand() % 254 + 1;
+        float b = rand() % 254 + 1;
+
+        std::map<std::string, float>
+            colorsCoded = NumbersGenerated((int)r, (int)g, (int)b);
+
+        for (int i = 0; i < 1; i++)
         {
-            std::cout << color.first << color.second << std::endl;
+            for (auto color : colorsCoded)
+            {
+                std::cout << color.first << color.second << std::endl;
+            }
+
+            std::cout << printMessage("App Has Completely Ran");
         }
     }
 
+    else
+    {
+        std::cout << printMessage("Failed to Find Code");
+    }
+}
+
+auto main() -> int
+{
+
+    setup();
     system("pause > 0");
     return 0;
 }
